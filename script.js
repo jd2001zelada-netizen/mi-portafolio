a04605d7ab9a69bece53293d391a583ae82b2c9a
jd2001zelada-netizen <jd2001zelada@gmail.com>
2026-09-12 11:26:45 -0500

    Mejoro diseño del portafolio: foto, habilidades, proyectos y contacto

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-console.log("¡Mi portafolio está funcionando!");
+console.log("¡Portafolio cargado correctamente!");
 
+// Animación suave al hacer click en los enlaces
+document.querySelectorAll('a[href^="#"]').forEach(enlace => {
+    enlace.addEventListener('click', function(e) {
+        e.preventDefault();
+        const destino = document.querySelector(this.getAttribute('href'));
+        if (destino) {
+            destino.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
+
+// Mensaje de bienvenida personalizado según la hora
 const hora = new Date().getHours();
 let saludo = "Hola";
 
@@ -11,4 +23,4 @@ if (hora < 12) {
     saludo = "Buenas noches";
 }
 
-console.log(saludo + ", bienvenido a mi portafolio");
+console.log(saludo + ", bienvenido a mi portafolio de Juan Daniel Zelada");
